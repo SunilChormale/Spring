449264ed1a71115e8482246bafc765f8b45133b5
SunilChormale <sunilschormale01@gmail.com>
2021-09-25 17:01:23 +0530

    SpringBoot::@Setter Injection and (No Setter and No Constructor Injection)::Sunil Chormale::Code Check-In

--- springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/BinarySearchImpl.java
+++ springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/BinarySearchImpl.java
@@ -3,16 +3,23 @@ package com.sunil.spring.basics.springbasics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// Setter Injection and (No Setter and No Constructor Injection)
+
 @Component
 public class BinarySearchImpl {
-
+	
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
+
+
+	//SETTER INJECTION
+	/*
+	 * public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+	 * this.sortAlgorithm = sortAlgorithm; }
+	 */
+
+	//In the (No Setter and No Constructor Injection) we don't need a constructor or setter function
 	
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
 		sortAlgorithm.sort(numbers);
