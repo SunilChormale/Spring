2babdedc92e41863d2a668fce304ef5a9c61a716
SunilChormale <sunilschormale01@gmail.com>
2021-09-25 23:31:26 +0530

    SpringBoot::@Primary(High Precedence) takes over the Dependency Naming via ClassName::Sunil Chormale::Code Check-In

--- springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/QuickSortAlgorithm.java
+++ springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/QuickSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.sunil.spring.basics.springbasics;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		//Logic for quick sort goes here
