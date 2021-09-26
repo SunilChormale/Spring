a85d93dbc9b1c6987d45af25b426ebfcc58ced82
SunilChormale <sunilschormale01@gmail.com>
2021-09-26 10:24:28 +0530

    SpringBoot::@Proxy (For creating Different Dependencies Instance(See Previous Commit for reference))::Sunil Chormale::Code Check-In

--- springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/scope/JdbcConnection.java
+++ springbasics/springbasics/src/main/java/com/sunil/spring/basics/springbasics/scope/JdbcConnection.java
@@ -2,10 +2,11 @@ package com.sunil.spring.basics.springbasics.scope;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class JdbcConnection {
 	
 	public JdbcConnection() {
